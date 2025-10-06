ddb4f907d70611a0e37a5db3a09ddba50d8de66f
savic116 <savic00085@gmail.com>
2025-10-06 19:00:19 +0900

    feat(random_dice): Add const constructor to RootScreen

--- lib/week04/random_dice/screen/root_screen.dart
+++ lib/week04/random_dice/screen/root_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 class RootScreen extends StatefulWidget {
-  
+  const RootScreen({Key? key}) : super(key: key);
 }
   @override
   Widget build(BuildContext context){
