a738b723d2846e200c833c0cd7011dc9b0a2f69c
savic116 <savic00085@gmail.com>
2025-10-06 18:38:59 +0900

    feat: Implement basic HomeScreen widget

---
+++ lib/week04/random_dice/screen/home_screen.dart
@@ -0,0 +1,10 @@
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  bool operator ==(Object other) {
+    // TODO: implement ==
+    return super == other;
+  }
+}
