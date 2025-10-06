bf94de878b15941a6fde7bba5894d614b37ac5b8
savic116 <savic00085@gmail.com>
2025-10-06 18:39:28 +0900

    feat(random_dice): Implement basic home screen UI

--- lib/week04/random_dice/screen/home_screen.dart
+++ lib/week04/random_dice/screen/home_screen.dart
@@ -3,8 +3,9 @@ class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
-  bool operator ==(Object other) {
-    // TODO: implement ==
-    return super == other;
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Text('Home Screen'),
+    )
   }
 }
