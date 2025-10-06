e970e6ec3712bb6f68047c6d2a73a671426b9515
savic116 <savic00085@gmail.com>
2025-10-06 18:58:49 +0900

    feat(random_dice): Implement root screen with bottom navigation

--- lib/week04/random_dice/main.dart
+++ lib/week04/random_dice/main.dart
@@ -1,7 +1,7 @@
 import 'package:myapp/week04/random_dice/screen/home_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:myapp/week04/random_dice/const/colors.dart';
-
+import 'package:myapp/week04/random_dice/screen/root_screen.dart';
 void main(){
   runApp(
     MaterialApp(
