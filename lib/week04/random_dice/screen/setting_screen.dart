a5fdea8b480e052477cb00d0e49d4c515e7c5af3
savic116 <savic00085@gmail.com>
2025-10-06 19:25:20 +0900

    feat(dice): Implement basic settings screen skeleton

---
+++ lib/week04/random_dice/screen/setting_screen.dart
@@ -0,0 +1,16 @@
+import 'package:myapp/week04/random_dice/const/colors.dart';
+import 'package:flutter/material.dart';
+
+class SettingScreen extends StatelessWidget{
+  final double threshold;
+
+  final ValueChanged<double> onThresholdChange;
+
+  const SettingScreen({
+    Key? key
+
+
+    required this.Threshold,
+    required this.onThresholdChange,
+  }) : super(key : key);
+}
