ff587cc58f39972960738c0e0b88cc26c2c85593
savic116 <savic00085@gmail.com>
2025-10-06 18:41:58 +0900

    feat: Implement main function to run HomeScreen

---
+++ lib/week04/random_dice/screen/main.dart
@@ -0,0 +1,11 @@
+import 'package:myapp/week04/random_dice/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+
+
+void main(){
+  runApp(
+    MaterialApp(
+      home: HomeScreen(),
+    ),
+  );
+}
